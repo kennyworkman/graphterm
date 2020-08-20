f265cd2e018be12b3f5625e38d3cf70c8584a4b6
Kenny Workman <kennyworkman@sbcglobal.net>
2020-08-20 14:22:26 -0700

    fix: directory structure

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ setup(name='graphterm',
       version='0.1',
       description='graphterm provides basic support for interactive drawings of directed acyclic graphs in ASCII.',
       url='http://github.com/kennyworkman/graphterm',
+      packages=['graphterm'],
       author='Kate Isaacs',
       )
