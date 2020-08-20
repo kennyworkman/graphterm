44cf8afbb36d2ff85e59bad72b43ed1a3e0f4968
Kenny Workman <kennyworkman@sbcglobal.net>
2020-08-20 13:55:53 -0700

    feat: minimal build tooling

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='graphterm',
+      version='0.1',
+      description='graphterm provides basic support for interactive drawings of directed acyclic graphs in ASCII.',
+      url='http://github.com/kennyworkman/graphterm',
+      author='Kate Isaacs',
+      )
